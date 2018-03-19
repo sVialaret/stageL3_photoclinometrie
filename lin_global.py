2e017766601376808211b96dbea6c04105f21631
Simon Vialaret <simon.vialaret@orange.fr>
2018-03-19 21:02:43 +0100

    changement de formalisme : matriciel --> vectoriel

--- lin_global.py
+++ lin_global.py
@@ -2,7 +2,6 @@
 
 import scipy.sparse as sp
 from scipy.sparse.linalg import spsolve
-from time import clock
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -16,33 +15,22 @@ N = nx * ny
 
 theta = np.pi / 5
 phi = np.pi / 3
-alpha = np.sin(theta) * np.cos(phi)
-beta = np.sin(theta) * np.sin(phi)
-gamma = np.cos(theta)
-lV = np.array([alpha, beta, gamma])
+theta_obs = 0
+phi_obs = 0
+lV = direction_eclairement((theta, phi), (theta_obs, phi_obs))
+
+alpha, beta, gamma = lV
 
 eps = 0.1
 
 dx = 1
 dy = 1
 
+nb_it = 1
+
 x = np.linspace(-nx / 2, nx / 2 - 1, nx)
 y = np.linspace(-ny / 2, ny / 2 - 1, ny)
 X, Y = np.meshgrid(y, x)
-
-# Z,E,V = generer_surface(Nx=nx, Ny=ny, forme=('volcan',20,20,0.5,0.2,0.5), reg = 0, lV=(theta,phi),obV=(0,0))
-# Z,E,V = generer_surface(Nx=nx, Ny=ny, forme=('trap',80,80,1,0.5), reg=0, lV=(theta,phi),obV=(0,0))
-Z, E, V = generer_surface(Nx=nx, Ny=ny, forme=('cone', 20,1), reg=0, lV=(theta, phi), obV=(0, 0))
-# Z,E,V = generer_surface(Nx=nx, Ny=ny, forme=('plateau',20,20,1), reg = 0, lV=(theta,phi),obV=(0,0))
-
-E_cp = E.copy()
-
-t2 = clock()
-
-print("surface generee")
-print(t2 - t1)
-
-t1 = clock()
 
 # gradient decentre
 
@@ -106,70 +94,72 @@ M_lap = M_dx.transpose().dot(M_dx) + M_dy.T.dot(M_dy)
 
 M = eps * M_lap + alpha * M_dx + beta * M_dy
 
-
-t2 = clock()
-
 print("matrice formee")
-print(t2 - t1)
 
-t1 = clock()
 
-nb_it = 1
+def grad(U): return (M_dx.dot(U), M_dy.dot(U))
+
+
+# Z,E,V = generer_surface(Nx=nx, Ny=ny, forme=('volcan',20,20,0.5,0.2,0.5), reg = 0, lV=(theta,phi),obV=(0,0))
+# Z,E,V = generer_surface(Nx=nx, Ny=ny, forme=('trap',80,80,1,0.5), reg=0, lV=(theta,phi),obV=(0,0))
+Z_mat = generer_surface(Nx=nx, Ny=ny, forme=('cone', 30, 5), reg=0)
+# Z,E,V = generer_surface(Nx=nx, Ny=ny, forme=('plateau',20,20,1), reg = 0, lV=(theta,phi),obV=(0,0))
+
+Z = np.reshape(Z_mat, N)
+
+E = eclairement(Z, lV, grad)
+
+E_cp = E.copy()
+E_cp_mat = np.reshape(E_cp, (nx, ny))
+
+V = np.sum(Z)
+
+print("surface generee")
 
 compt = 0
 
-Z_appr = np.zeros((nx, ny))
+Z_appr = np.zeros(N)
 
 plt.figure(-5)
-plt.imshow(E_cp, cmap='gray')
+plt.imshow(E_cp_mat, cmap='gray')
 
 while compt < nb_it:
 
-	compt += 1
+    compt += 1
 
-	Z_gradx, Z_grady = np.gradient(Z_appr)
-	corr = np.sqrt(1 + Z_gradx**2 + Z_grady**2)
-	E = E_cp * corr - gamma
-	    
-	F = np.reshape(E, N)
+    Z_gradx, Z_grady = grad(Z_appr)
+    corr = np.sqrt(1 + Z_gradx**2 + Z_grady**2)
+    E = E_cp * corr - gamma
 
-	F[:ny] = [0] * ny
-	F[-ny:] = [0] * ny
+    for i in range(ny):
+        E[i] = 0
+        E[-(i + 1)] = 0
 
-	for i in range(1, nx - 1):
-	    F[ny * i] = 0
-	    F[ny * (i + 1) - 1] = 0
+    for j in range(nx):
+        E[j * ny] = 0
+        E[(j + 1) * ny - 1] = 0
 
-	Z_appr_vect = spsolve(M,F)
+    Z_appr = spsolve(M, E)
 
-	Z_appr_n = np.reshape(Z_appr_vect, (nx, ny))
+    E_appr = eclairement(Z_appr, lV, grad)
+    Z_appr_mat = np.reshape(Z_appr, (nx, ny))
+    E_appr_mat = np.reshape(E_appr, (nx, ny))
 
-	E_appr = eclairement(Z_appr_n, lV)
+    fig = plt.figure(10 * compt)
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(X, Y, Z_appr_mat, rstride=2, cstride=2, linewidth=1)
+    ax.plot_wireframe(X, Y, Z_mat, rstride=2, cstride=2, linewidth=1, color='r')
 
-	fig = plt.figure()
-	ax = fig.gca(projection='3d')
-	ax.plot_surface(X,Y,Z_appr_n,rstride=2,cstride=2,linewidth=1)
-	ax.plot_wireframe(X,Y,Z,rstride=2,cstride=2,linewidth=1,color='r')
-	plt.title(compt)
+    # fig = plt.figure(10 * compt + 1)
+    # ax = fig.gca(projection='3d')
+    # ax.plot_surface(X,Y,Z_appr_n - Z_appr,rstride=2,cstride=2,linewidth=1)
 
-	# fig = plt.figure()
-	# ax = fig.gca(projection='3d')
-	# ax.plot_surface(X,Y,Z_appr_n - Z_appr,rstride=2,cstride=2,linewidth=1)
-	# plt.title(10*compt +1)
-	
-	E_appr = (E_appr - np.min(E_appr))/(np.max(E_appr)-np.min(E_appr))
+    plt.figure(10 * compt + 2)
+    plt.imshow(E_appr_mat, cmap='gray')
 
-	plt.figure(100*compt)
-	plt.imshow(E_appr,cmap='gray')
+    print(comparer_eclairement(E_cp, E_appr))
+    V_appr = np.sum(Z_appr)
+    print(np.abs(V - V_appr) / V)
 
-	print(comparer_eclairement(E_cp,E_appr))
-	Z_appr = Z_appr_n
-	V_appr = np.sum(Z_appr)
-	print(V, V_appr, np.abs(V - V_appr) / V)
-
-t2 = clock()
-
-print("affichage ok")
-print(t2 - t1)
 
 plt.show()
