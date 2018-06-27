510aeade274e6987527ce15bf4205cbb328f66e1
Victor LEGER <victor158@hotmail.fr>
2018-06-27 23:40:21 +0200

    Code nettoyé

--- rotation_surface.py
+++ rotation_surface.py
@@ -3,7 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from libSFS import direction_eclairement, generer_surface, eclairement, points_critiques, height, comp_connexes, rearrange
+from libSFS import *
 from time import clock
 from copy import deepcopy
 
@@ -65,10 +65,6 @@ def FMM(I,z0,dx,dy,masque):
     T=deepcopy(Q_bis)
     i=0
     while (T!=1).any(): # génération de la solution
-        # if i%100==0:
-        #     fig = plt.figure(30+i//100)
-        #     ax = fig.gca(projection='3d')
-        #     ax.plot_surface(X,Y,z,rstride=2,cstride=2,linewidth=1)
         for x in range(1,nx-1):
             for y in range(1,ny-1):
                 if T[x,y]==0:
@@ -111,38 +107,14 @@ def regularisation_maillage(X,Y,Z):
             Z_reg[i,j] = np.sum(Z[indX[:4], indY[:4]])/ 4
     return Z_reg
 
-im1=plt.imread("img/3/non_sym_mask.png")[:,:,0]
-im2=plt.imread("img/3/non_sym.png")[:,:,0]
-(nx,ny)=im1.shape
-
-
-## "Compression" de l'image réelle
-
-# comp=2 # doit être entier
-# 
-# nx=nx-nx%comp
-# ny=ny-ny%comp
-# im1=im1[0:nx,0:ny]
-# im2=im2[0:nx,0:ny]
-# nx=nx//comp
-# ny=ny//comp
-# IM1=deepcopy(im1)
-# IM2=deepcopy(im2)
-# im1=np.zeros((nx,ny))
-# im2=np.zeros((nx,ny))
-# for i in range(nx):
-#     for j in range(ny):
-#         im1[i,j]=np.sum(IM1[comp*i:comp*(i+1),comp*j:comp*(j+1)])/comp**2
-#         im2[i,j]=np.sum(IM2[comp*i:comp*(i+1),comp*j:comp*(j+1)])/comp**2
-
 ## Parametres du probleme
 
-# nx = 64
-# ny = 64
+nx = 64
+ny = 64
 
 delta=0.01
 
-theta = np.pi/5
+theta = np.pi/10
 phi = np.pi/2
 theta_obs = 0
 phi_obs = 0
@@ -157,39 +129,12 @@ x_mesh = np.linspace(-(nx-1)/2, (nx-1)/2, nx)
 y_mesh = np.linspace(-(ny-1)/2, (ny-1)/2, ny)
 X,Y = np.meshgrid(y_mesh,x_mesh)
 
-# Z = generer_surface(Nx=nx, Ny=ny, forme=('cone',30,15), reg=0)
+Z = generer_surface(Nx=nx, Ny=ny, forme=('cone',30,15), reg=0)
 # Z = generer_surface(Nx=nx, Ny=ny, forme=('trap',10,20,5,10), reg = 0)
-# Z = generer_surface(Nx=nx, Ny=ny, forme=('plateau',30,30,5), reg = 0)
+# Z = generer_surface(Nx=nx, Ny=ny, forme=('plateau',20,20,5), reg = 1)
 
-# V=np.sum(Z)
-# I=eclairement(Z,lV,np.gradient)
-
-## Application du masque à l'image réelle
-
-for i in range(nx):
-    for j in range(ny):
-        if im1[i,j]<1 and im1[i,j]>0:
-            im1[i,j]=1
-
-im2=(1-im1)*im2
-
-# im2=im2/np.max(im2)
-
-im2=((im2-np.min(im2))*(1-np.sqrt(alpha**2+beta**2))/(np.max(im2)-np.min(im2))+np.sqrt(alpha**2+beta**2))*(1-im1)
-
-I=im2+im1*np.cos(theta)
-
-# for i in range(nx):
-#     for j in range(ny):
-#         if I[i,j]<np.sin(theta):
-#             I[i,j]=np.sin(theta)+0.001
-
-plt.figure(10)
-plt.imshow(im1)
-plt.figure(11)
-plt.imshow(im2)
-plt.figure(12)
-plt.imshow(I)
+V=np.sum(Z)
+I=eclairement(Z,lV,np.gradient)
 
 ##
 
@@ -203,16 +148,15 @@ else :
 plt.figure(9)
 plt.imshow(cond)
 
-masque = im1#np.zeros((nx,ny))
-# for i in range(nx):
-#     for j in range(ny):
-#         if np.sqrt((nx/2-i)**2+(ny/2-j)**2)>30:
-#             masque[i,j]=1
+masque = np.zeros((nx,ny)) 
+for i in range(nx): 
+    for j in range(ny): 
+        if np.sqrt((nx/2-i)**2+(ny/2-j)**2)>30: 
+            masque[i,j]=1
 
 plt.figure(7)
 plt.imshow(masque)
 
-# Z1=deepcopy(Z)
 Z1=np.zeros((nx,ny))
 
 Z_mesh = np.array([X,Y,Z1])
@@ -222,19 +166,15 @@ x_mesh_reg = np.linspace(np.min(Z_rot[1]), np.max(Z_rot[1]), nx)
 y_mesh_reg = np.linspace(np.min(Z_rot[0]), np.max(Z_rot[0]), ny)
 X_reg,Y_reg = np.meshgrid(y_mesh_reg, x_mesh_reg)
 
-for i in range(3):
-    # Z_n
+for i in range(6):
+    
+    # Z1  ->  Z_n
     
     Z_mesh = np.array([X,Y,Z1])
     Z_rot = rotation_Z(Z_mesh, -theta)
     Z_reg = regularisation_maillage(Z_rot[0],Z_rot[1],Z_rot[2])
-    
-    fig = plt.figure(30)
-    ax = fig.gca(projection='3d')
-    ax.axis('off')#'equal')
-    ax.plot_surface(X_reg, Y_reg, Z_reg, rstride=1, cstride=1, linewidth=0, color='#acc2d9')#,rstride=2,cstride=2,linewidth=1, color='r')
 
-    # Z_n_tilde
+    # Z_reg  ->  Z_n_tilde
     
     Z0 = np.zeros((nx,ny))
     
@@ -248,89 +188,34 @@ for i in range(3):
     
     z = FMM(I_rot_reg,Z0_reg,dx_rot,dy,masque_rot_reg)
     
-    # Z_n+1_tilde
+    # z  ->  Z_n+1_tilde
     
     Z_mesh = np.array([X_reg,Y_reg,z])
     Z_rot = rotation_Z(Z_mesh, theta)
     Z1 = regularisation_maillage(Z_rot[0],Z_rot[1],Z_rot[2])
-    
-    # Z_n+1
-    # fig = plt.figure(i+20)
-    # ax = fig.gca(projection='3d')
-    # ax.axis('equal')
-    # ax.plot_surface(X, Y, Z1,rstride=2,cstride=2,linewidth=1, color='r')
     print(i)
     
 
-I_sim = eclairement(Z1*(Z1>0),lV,np.gradient)
+I_sim = eclairement(Z1,lV,np.gradient)
 
 fig = plt.figure(1)
 ax = fig.gca(projection='3d')
-ax.axis('off')#ax.axis('equal')
-ax.set_zlim3d(0,8)
-ax.plot_surface(X, Y, (Z1*(Z1>0)), rstride=1, cstride=1, linewidth=0, color='#acc2d9')#ax.plot_surface(X, Y, Z1*(Z1>0),rstride=2,cstride=2,linewidth=1, color='r')
-#ax.plot_wireframe(X, Y, Z, rstride=5, cstride=5, linewidth=1, color='r')
-
-# fig = plt.figure(10)
-# ax = fig.gca(projection='3d')
-# ax.axis('off')
-# ax.set_zlim3d(-10,12)
-# ax.plot_surface(Z_rot[0], Z_rot[1], Z_rot[2], rstride=1, cstride=1, linewidth=0, color='#acc2d9')
-# ax.plot_wireframe(X, Y, Z_mat, rstride=5, cstride=5, linewidth=1, color='r')
-# ax.plot_surface(X, Y, (Z_appr_mat * (Z_appr_mat >= 0))[::-1,:], rstride=1, cstride=1, linewidth=0, color='#acc2d9')
+plt.axis('off')
+ax.plot_surface(X, Y, Z1, rstride=1, cstride=1, linewidth=0, color='#acc2d9')
+ax.plot_wireframe(X, Y, Z, rstride=5, cstride=5, linewidth=1, color='r')
 
 plt.figure(2)
 plt.imshow(I)
 
-# Z_mesh = np.array([X,Y,Z])
-# Z_rot = rotation_Z(Z_mesh, -theta)
-# Z_reg = regularisation_maillage(Z_rot[0],Z_rot[1],Z_rot[2])
-# I_rot_vrai = eclairement(Z_reg, (0,0,1), np.gradient)
-# plt.figure(3)
-# plt.imshow(I_rot_vrai)
-
 plt.figure(4)
 plt.imshow(I_rot_reg)
-
-pts_crit=np.zeros((nx,ny))
-for i in range(nx):
-    for j in range(ny):
-        if abs(I[i,j]-1) < 0.0001:
-            pts_crit[i,j]=1
 
 plt.figure(5)
 plt.imshow(I_sim)
 
-plt.figure(6)
-plt.imshow(pts_crit)
-
-# Z0 = np.zeros((nx,ny))
-# 
-# Z0_mesh = np.array([X,Y,Z0])
-# Z0_rot = rotation_Z([X,Y,Z0], -theta)
-# Z0_reg = regularisation_maillage(Z0_rot[0],Z0_rot[1],Z0_rot[2])
-# Z_test = FMM(I_rot_vrai,Z0_reg,dx,dy,masque)
-# 
-# Z_mesh = np.array([X_reg,Y_reg,Z_test])
-# Z_rot = rotation_Z(Z_mesh, theta)
-# Z_test2 = regularisation_maillage(Z_rot[0],Z_rot[1],Z_rot[2])
-# 
-# I_test = eclairement(Z_reg,lV,np.gradient)
-# 
-# plt.figure(7)
-# plt.imshow(I_test)
-
-fig = plt.figure(17)
-ax = fig.gca(projection='3d')
-ax.axis('equal')
-ax.plot_surface(X_reg,Y_reg,z,rstride=2,cstride=2,linewidth=1, color='r')
-
-fig = plt.figure(18)
-ax = fig.gca(projection='3d')
-ax.axis('equal')
-ax.plot_surface(X_reg,Y_reg,z*(z>Z0_reg)+Z0_reg*(z<=Z0_reg),rstride=2,cstride=2,linewidth=1, color='r')
-
 plt.show()
 
-#v=np.sum(Z1)
-# print(v)
+v=np.sum(Z1)
+print(abs(v-V)/V)
+
+print(comparer_eclairement(I, I_sim))
